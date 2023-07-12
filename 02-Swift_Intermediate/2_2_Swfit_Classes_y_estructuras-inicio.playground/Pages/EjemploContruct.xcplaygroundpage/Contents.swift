813e420c9455e0649d2638830eb7538cd991fb37
Ana Reguera <amarebu1999@gmail.com>
2023-07-12 10:22:35 +0200

    Esto es un ejemplo de constructor

---
+++ 02-Swift_Intermediate/2_2_Swfit_Classes_y_estructuras-inicio.playground/Pages/EjemploContruct.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+import Foundation
+
+struct Bootcamp {
+    var id: String
+    var name: String?
+    
+    // El struct crea un inicializador por defecto, algo que no hacen las clases
+}
+
+let boot = Bootcamp(id: "Esto es un id")
